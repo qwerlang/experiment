afdd88b2d1730c8fc619366e00f86f687840056a
langxiong <2926816345@qq.com>
2022-06-20 22:41:08 +0800

    0.0.5

--- src/main/java/com/example/experiment/ExperimentApplication.java
+++ src/main/java/com/example/experiment/ExperimentApplication.java
@@ -8,10 +8,10 @@ public class ExperimentApplication {
 
     public static void main(String[] args) {
         System.out.println(
-                "sima"+"good"
+                "sima"+"good"+"sfhgiso"
         );
         System.out.println("你好傻逼"+"nimade");
-
+        System.out.println("我真的服了你个狗");
         SpringApplication.run(ExperimentApplication.class, args);
     }
 
